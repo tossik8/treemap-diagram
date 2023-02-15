6bc7e4d0bb504c4842634f8dc7051c8545930c16
Nikita <toropov.nikita2004@gmail.com>
2023-02-15 19:29:14 +0100

    Add a legend for campaigns

---
+++ js/legend-campaign-items.js
@@ -0,0 +1,80 @@
+const firstColumn = [
+  {
+    text: "Product Design",
+    colour: "#4c92c3"
+  },
+  {
+    text:"Technology",
+    colour: "#ffc993"
+  },
+  {
+    text: "Gaming Hardware",
+    colour: "#de5253"
+  },
+  {
+    text: "Television",
+    colour: "#d1c0dd"
+  },
+  {
+    text:"Food",
+    colour:"#e992ce"
+  },
+  {
+    text:"Apparel",
+    colour:"#d2d2d2"
+  }
+];
+const secondColumn = [
+      {
+        text:"Tabletop Games",
+        colour:"#bed2ed"
+      },
+      {
+        text:"Hardware",
+        colour:"#56b356"
+      },
+      {
+        text:"Narative Film",
+        colour:"#ffadab"
+      },
+      {
+        text:"Web",
+        colour:"#a3786f"
+      },
+      {
+        text:"Games",
+        colour:"#f9c5db"
+      },
+      {
+        text:"Art",
+        colour:"#c9ca4e"
+      }
+];
+const thirdColumn = [
+      {
+        text:"Video Games",
+        colour:""
+      },
+      {
+        text:"Sound",
+        colour:""
+      },
+      {
+        text:"3D Printing",
+        colour:""
+      },
+      {
+        text:"Wearables",
+        colour:"#d0b0a9"
+      },
+      {
+        text:"Sculpture",
+        colour:"#999999"
+      },
+      {
+        text:"Gadgets",
+        colour:"#e2e2a4"
+      }
+];
+
+export {firstColumn, secondColumn, thirdColumn};
